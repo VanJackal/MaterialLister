58b17116f7cc275ec21eeacba2bde52392372e93
Jackal <v@njackal.com>
2024-05-29 12:24:01 -0500

    initial setup

---
+++ src/client/java/com/njackal/materials/MaterialListerClient.java
@@ -0,0 +1,10 @@
+package com.njackal.materials;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MaterialListerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
